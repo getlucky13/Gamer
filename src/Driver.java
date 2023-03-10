dd0a60b2c89935a6ed16a28eacd00166cccf043c
Ron Rizzo <churchmouth13@gmail.com>
2023-03-09 22:55:57 -0500

    Fleshes out Driver.java, including comments

--- src/Driver.java
+++ src/Driver.java
@@ -2,21 +2,35 @@ import java.util.ArrayList;
 
 public class Driver {
     public static void main(String[] args) throws Exception {
+        /**
+         * Creates two Accolade objects as well as an ArrayList to hold them, adds
+         * both to ArrayList
+         */
         Accolade champions2020 = new Accolade("VCT Finals", "First Place", 2020);
         Accolade champions2021 = new Accolade("VCT Finals", "First Place", 2021);
         ArrayList<Accolade> accolades = new ArrayList<>();
         accolades.add(champions2020);
         accolades.add(champions2021);
 
+        /**
+         * Creates an EsportsAthlete object, demonstrates toString
+         */
         EsportsAthlete ron = new EsportsAthlete("Ron Rizzo", "Philadlephia", 
         "Valorant", 31, "Fnatic", "Controller", "Omen", accolades, true);
         System.out.println(ron.toString());
         System.out.println();
 
+        /**
+         * Creates a new Accolade and adds it to accolades field of the ron EsportAthlete
+         * object, runs toString to demonstrate success
+         */
         Accolade champions2022 = new Accolade("VCT Finals", "First Place", 2022);
         ron.addAccolade(champions2022);
         System.out.println(ron.toString());
         
+        /**
+         * Creates an EsportStreamer object and prints it
+         */
         EsportStreamer bob = new EsportStreamer("Bob Coccia", "Philadlephia", 
         "Valorant", 33, "100 Thieves", "Duelist", "Reyna", 
         accolades, true, "Twitch", "Streaming");
@@ -24,19 +38,30 @@ public class Driver {
         System.out.println(bob.toString());
         System.out.println();
         
-        
+        /**
+         * Removes an accolade from ron and prints ron and bob, demonstrating that even 
+         * though both objects were constructed with the same ArrayList for their accolades
+         * field, removing from one objects accolades field does not impact the other
+         */
         ron.removeAccolade(champions2020);
         System.out.println(ron.toString());
         System.out.println();
         System.out.println(bob.toString());
+        System.out.println();
         
-        Streamer drLupo = new Streamer("Ben Lupo", "Nebraska", "Escape From Tarkov", 
+        /**
+         * Creates three new objects, one Streamer, one Casual, and one Speedrunner
+         */
+        Streamer drLupo = new Streamer("DrLupo", "Nebraska", "Escape From Tarkov", 
         36, "None", "YouTube");
         Casual dale = new Casual("Dale Nicklas", "Hatboro", "Overwatch 2", 
         31, "PC");
         SpeedRunner lilAggy = new SpeedRunner("LilAggy", "Unknown", "Elden Ring", 
-        28, "GGTalen", "PC", "Fog Gate Randomizer", "1hr15min", "None");
+        28, "GGTalen", "Twitch", "Fog Gate Randomizer", "1hr15min", "None");
     
+        /**
+         * Creates an ArrayList of Gamer objects, and adds all current objects to it
+         */
         ArrayList<Gamer> gamers = new ArrayList<>();
         gamers.add(ron);
         gamers.add(bob);
@@ -44,33 +69,20 @@ public class Driver {
         gamers.add(drLupo);
         gamers.add(lilAggy);
 
-        boolean wut = drLupo instanceof Gamer;
-        if(wut == true) {
-            System.out.println(drLupo.toString());
-        }
-        for(Gamer gamer: gamers) {
-            if(gamer instanceof Streamer){
-                System.out.println(gamer.toString());
+        /**
+         * Loops through gamers, printing each objects name. If the object is an instance
+         * of the Streamer class or its children, prints the objects name field as well as
+         * platform. 
+         */
+        for(Gamer person: gamers) {
+            System.out.print(person.getName());
+            if(person instanceof Streamer){
+                System.out.println(": " + ((Streamer)person).getPlatform());
+            } else {
+                System.out.println();
             }
         }
-
-
     }
 }
 
-/*
- * To Do:
- *      *Documentation done
- * Stretch Goals:
- *     *Class for RecordsHeld
- *          *Update Gamer with method for converting to string
- *          *Update SpeedRunner RecordsHeld field
- *     *Team interface
- *     *EsportsTeam Class
- *         *Game specific subclasses?
- *         *All games that org has teams for in same obj?
- *         *Still need to decide
- *      *Encase ArrayList prints in [] for viewing in toString methods
- *      *Maybe create a print method that is essentially toString but formatted for cmdline
- */
 
